4a115b672c673f8b32b3ca675995292ab368296f
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-09-20 13:25:09 -0400

    Create concat_mc.py

---
+++ concat_mc.py
@@ -0,0 +1,37 @@
+# Ethan Lawrence
+# September 20 2024
+# Concatination Check
+
+# Task 1
+# Input
+first_name = 'Ethan'
+current_game = 'FTL'
+
+# Process
+task1_msg = first_name + '\s Favorite game is ' + current_game + '.'
+
+# Output
+print(task1_msg)
+
+print() # Spacing
+
+
+# Task 2
+print('What i\'ve learned about concatination: ')
+print('Concatination uses "+" to combine 2 STRINGS together')
+print('Concatonation can work with both string containing variables' + ' AND regular strings' + ' BUT not anything else')
+
+print() # Spacing
+
+
+# Task 3
+# Input
+whopper_price = 5.99 # I am a float!
+
+# Process
+price_as_string = '$' + str(whopper_price)
+
+# Output
+print('The price of a "Whopper with cheese" is: ' + price_as_string)
+
+print() # Spacing
